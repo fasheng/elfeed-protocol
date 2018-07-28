6eda7edf0c7de3edc301e0bfcd19fe541677dcc1
Jonathan Jin <jjin082693@gmail.com>
2018-07-28 14:37:35 -0400

    tests: owncloud: fixtures to files

--- test/elfeed-protocol-owncloud-test.el
+++ test/elfeed-protocol-owncloud-test.el
@@ -2,89 +2,17 @@
 (require 'ert)
 (require 'elfeed)
 
-(defvar elfeed-protocol-owncloud-test-feeds-json
-  "{
-  \"starredCount\": 1,
-  \"feeds\": [
-    {
-      \"id\": 1,
-      \"url\": \"http://www.example.com/feed/\",
-      \"title\": \"Feed 1\",
-      \"faviconLink\": \"http://www.example.com/favicon.ico\",
-      \"added\": 1492783825,
-      \"folderId\": 11,
-      \"unreadCount\": 2,
-      \"ordering\": 0,
-      \"link\": \"http://www.example.com/\",
-      \"pinned\": false,
-      \"updateErrorCount\": 0,
-      \"lastUpdateError\": null
-    },
-    {
-      \"id\": 2,
-      \"url\": \"http://www.example2.com/rss.jsp\",
-      \"title\": \"Feed 2\",
-      \"faviconLink\": \"\",
-      \"added\": 1492784045,
-      \"folderId\": 12,
-      \"unreadCount\": 0,
-      \"ordering\": 0,
-      \"link\": \"http://www.example2.com/\",
-      \"pinned\": false,
-      \"updateErrorCount\": 0,
-      \"lastUpdateError\": \"\"
-    }
-  ],
-  \"newestItemId\": 2
-}")
+(defvar elfeed-protocol-owncloud-fixtures-dir "./fixtures/owncloud/")
 
-(defvar elfeed-protocol-owncloud-test-entries-json
-  "{
-  \"items\": [
-    {
-      \"id\": 1,
-      \"guid\": \"646062e8942103b24ae2470b552577dd\",
-      \"guidHash\": \"646062e8942103b24ae2470b552577dd\",
-      \"url\": \"http://www.example.com/test.html?from=rss\",
-      \"title\": \"Entry 1\",
-      \"author\": \"www.example.com\",
-      \"pubDate\": 1493260874,
-      \"body\": \"<p>body 1...</p>\",
-      \"enclosureMime\": null,
-      \"enclosureLink\": null,
-      \"feedId\": 1,
-      \"unread\": false,
-      \"starred\": false,
-      \"lastModified\": 1493260874,
-      \"rtl\": false,
-      \"fingerprint\": \"d20ec9d7c728c6486f418e3d725b03ad\",
-      \"contentHash\": \"3e0d73ae064930f5f0c0eebb3258c100\"
-    },
-    {
-      \"id\": 2,
-      \"guid\": \"06b3c14d9433ea9bbdea131bd37168d8\",
-      \"guidHash\": \"06b3c14d9433ea9bbdea131bd37168d8\",
-      \"url\": \"http://www.example.com/test2.html?from=rss\",
-      \"title\": \"Entry 2\",
-      \"author\": \"www.example.com\",
-      \"pubDate\": 1493260874,
-      \"body\": \"<p>body 2...</p>\",
-      \"enclosureMime\": \"video/webm\",
-      \"enclosureLink\": \"http://www.example/test.webm\",
-      \"feedId\": 2,
-      \"unread\": true,
-      \"starred\": true,
-      \"lastModified\": 1493260874,
-      \"rtl\": false,
-      \"fingerprint\": \"6fb19b56a1c30f8e952af57b809d3dec\",
-      \"contentHash\": \"61206dd01bba2282c261215bfc24c6e5\"
-    }
-  ]
-}")
+(defvar elfeed-protocol-owncloud-fixture-feeds
+  (concat elfeed-protocol-owncloud-fixtures-dir "feeds.json"))
+
+(defvar elfeed-protocol-owncloud-fixture-entries
+  (concat elfeed-protocol-owncloud-fixtures-dir "entries.json"))
 
 (ert-deftest elfeed-protocol-owncloud-parse-feeds ()
   (with-temp-buffer
-    (insert elfeed-protocol-owncloud-test-feeds-json)
+    (insert-file-contents elfeed-protocol-owncloud-fixture-feeds)
     (goto-char (point-min))
     (with-elfeed-test
      (let* ((url "https://user:pass@myhost.com:443")
@@ -121,7 +49,7 @@
 
 (ert-deftest elfeed-protocol-owncloud-parse-entries ()
   (with-temp-buffer
-    (insert elfeed-protocol-owncloud-test-feeds-json)
+    (insert-file-contents elfeed-protocol-owncloud-fixture-feeds)
     (goto-char (point-min))
     (with-elfeed-test
      (let* ((url "https://user:pass@myhost.com:443")
@@ -132,7 +60,7 @@
                                         ("http://www.example2.com/rss.jsp" tag2)))))
             (elfeed-protocol-owncloud-feeds (elfeed-protocol-owncloud--parse-feeds url)))
        (with-temp-buffer
-         (insert elfeed-protocol-owncloud-test-entries-json)
+         (insert-file-contents elfeed-protocol-owncloud-fixture-entries)
          (goto-char (point-min))
          (let* (
                 (entries (elfeed-protocol-owncloud--parse-entries url))
